51faf270ea09f7733449b07a84da61d185a0acb7
forthright47 <k.tuhinjaman@gmail.com>
2026-03-28 22:01:15 +0600

    added the selected players in the selected toggle menu with card design and styled according to design

--- src/components/SelectedPlayers/SelectedPlayers.jsx
+++ src/components/SelectedPlayers/SelectedPlayers.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { FaUser } from 'react-icons/fa';
+import { MdDeleteForever } from 'react-icons/md';
 
 const SelectedPlayers = ({selectedPlayers}) => {
     console.log(selectedPlayers);
     return (
         <div>
-            Selected Players
+            {selectedPlayers.map((player, i)=>{
+                return <div key={i} className='flex items-center gap-6 justify-between p-6 rounded-2xl border my-3'>
+                    <div className='flex items-center gap-6'>
+                        <img src={player.playerImg} alt={player.playerName} className='h-[60px] w-auto rounded-md' />
+                        <div>
+                            <h2 className='flex gap-2 items-center font-semibold text-xl'><FaUser />{player.playerName}</h2>
+                            <p>{player.playerType}</p>
+                        </div>
+                    </div>
+                    <button className='btn text-red-500'>
+                        <MdDeleteForever />
+                    </button>
+                </div>
+            })}
         </div>
     );
 };
