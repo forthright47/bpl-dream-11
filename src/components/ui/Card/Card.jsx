1bac782474365ab743a5713f4ca383dc1ff59051
forthright47 <k.tuhinjaman@gmail.com>
2026-03-28 13:47:53 +0600

    make the card component reusable by shifting it to the ui folder and make the grid cards responsive

---
+++ src/components/ui/Card/Card.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { FaFlag, FaUser } from 'react-icons/fa';
+
+const Card = ({player}) => {
+    return (
+        <div className="card bg-base-100 shadow-sm">
+            <figure className="h-70 overflow-hidden p-3">
+                <img className="w-full h-full object-cover rounded-xl"
+                    src={player.playerImg} />
+            </figure>
+            <div className="card-body -m-3">
+                <h2 className="card-title"><FaUser /> {player.playerName}</h2>
+                <div className='flex justify-between items-center'>
+                    <div className='flex items-center gap-2'>
+                        <FaFlag className='text-black/50' />
+                        <p className='text-black/50'>{player.playerCountry}</p>
+                    </div>
+                    <button className='btn'>{player.playerType}</button>
+                </div>
+
+                <div className="divider -mt-1.5 -mb-1"></div>
+
+                <p className='font-bold'>Rating: {player.rating}</p>
+                <div className='flex justify-between items-center'>
+                    <p className='font-bold'>{player.battingStyle}</p>
+                    <p className='font-bold text-right'>{player.bowlingStyle}</p>
+                </div>
+                <div className="card-actions justify-between items-center">
+                    <p className='font-bold'>Price: ${player.price}</p>
+                    <button className="btn">Choose Player</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
