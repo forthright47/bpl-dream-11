36bf93ef81b5ee5308cf1028afe62abb0c2f8b57
forthright47 <k.tuhinjaman@gmail.com>
2026-03-28 14:15:32 +0600

    implemented button toggling and state management based on selectedType: Available or Selected

--- src/components/AvailablePlayers/AvailablePlayers.jsx
+++ src/components/AvailablePlayers/AvailablePlayers.jsx
@@ -4,7 +4,7 @@ import Card from '../ui/Card/Card';
 const AvailablePlayers = ({ players }) => {
     return (
         <div>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
                 {
                     players.map(player => {
                         return (
--- src/components/Players/Players.jsx
+++ src/components/Players/Players.jsx
@@ -1,4 +1,4 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import AvailablePlayers from '../AvailablePlayers/AvailablePlayers';
 
 const Players = ({playersPromise}) => {
@@ -7,8 +7,21 @@ const Players = ({playersPromise}) => {
     const players = use(playersPromise);
     console.log(players);
 
+    const [selectedType, setSelectedType] = useState("Available");
+
     return (
-        <div className='container mx-auto'>
+        <div className='container mx-auto my-8'>
+            <div className='flex justify-between gap-4 items-center mb-5'>
+                {selectedType === "Available" ? <h2 className='font-bold text-3xl'>Available Players</h2> : <h2 className='font-bold text-2xl'>Selected Players (0/11)</h2>}
+                <div>
+                    <button
+                    onClick={()=>setSelectedType("Available")}
+                    className={`btn ${selectedType === "Available" ? "bg-[#E7FE29]" : ""} rounded-r-none rounded-l-xl`}>Available</button>
+                    <button
+                    onClick={()=>setSelectedType("Selected")}
+                    className={`btn ${selectedType === "Selected" ? "bg-[#E7FE29]" : ""} rounded-l-none rounded-r-xl`}>Selected (0)</button>
+                </div>
+            </div>
             <AvailablePlayers players={players}/>
         </div>
     );
