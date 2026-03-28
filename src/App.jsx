07c8e19eaa98de428b6a7277a04557cf6ffd53ff
forthright47 <k.tuhinjaman@gmail.com>
2026-03-28 11:28:24 +0600

    added some players data as JSON and get the response from the promise

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
+import { Suspense } from 'react';
 import './App.css'
 import Banner from './components/Banner/Banner'
 import Navbar from './components/Navbar/Navbar'
+import Players from './components/Players/Players';
+
+const playersData = async() => {
+  const res = await fetch("/players.json");
+  return res.json();
+}
 
 function App() {
+
+  const playersPromise = playersData();
+
   return (
     <div>
       <Navbar />
       <Banner />
+      <Suspense fallback={<span className="loading loading-dots loading-xl"></span>
+}>
+        <Players playersPromise={playersPromise}/>
+      </Suspense>
     </div>
   )
 }
