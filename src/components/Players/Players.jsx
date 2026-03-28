d0997c2798d485b6c59fc8e523a27c4073e99604
forthright47 <k.tuhinjaman@gmail.com>
2026-03-29 00:06:30 +0600

    fixed some inconsistencies in the players component

--- src/components/Players/Players.jsx
+++ src/components/Players/Players.jsx
@@ -3,31 +3,37 @@ import AvailablePlayers from '../AvailablePlayers/AvailablePlayers';
 import SelectedPlayers from '../SelectedPlayers/SelectedPlayers';
 
 const Players = ({ playersPromise, setCoin, coin }) => {
-    console.log(playersPromise);
 
     const players = use(playersPromise);
-    console.log(players);
 
     const [selectedType, setSelectedType] = useState("Available");
-
     const [selectedPlayers, setSelectedPlayers] = useState([]);
 
     return (
-        <div className='container mx-auto my-8'>
-            <div className='flex flex-col sm:flex-row self-end sm:self-auto'>
-                <button
-                    onClick={() => setSelectedType("Available")}
-                    className={`btn text-sm px-3 sm:text-base sm:px-4 ${selectedType === "Available" ? "bg-[#E7FE29]" : ""} rounded-xl sm:rounded-r-none sm:rounded-l-xl`}>
-                    Available
-                </button>
-                <button
-                    onClick={() => setSelectedType("Selected")}
-                    className={`btn text-sm px-3 sm:text-base sm:px-4 ${selectedType === "Selected" ? "bg-[#E7FE29]" : ""} rounded-xl sm:rounded-l-none sm:rounded-r-xl`}>
-                    Selected ({selectedPlayers.length})
-                </button>
+        <div className='container mx-auto px-4 my-10'>
+            {/* Header & Toggle */}
+            <div className='flex flex-col sm:flex-row justify-between gap-3 sm:gap-4 items-start sm:items-center mb-5'>
+                {selectedType === "Available"
+                    ? <h2 className='font-bold text-xl lg:text-3xl'>Available Players</h2>
+                    : <h2 className='font-bold text-xl lg:text-2xl'>Selected Players ({selectedPlayers.length}/{players.length})</h2>
+                }
+                <div className='flex flex-col sm:flex-row self-end sm:self-auto'>
+                    <button
+                        onClick={() => setSelectedType("Available")}
+                        className={`btn text-sm px-3 sm:text-base sm:px-4 ${selectedType === "Available" ? "bg-[#E7FE29]" : ""} rounded-xl sm:rounded-r-none sm:rounded-l-xl`}>
+                        Available
+                    </button>
+                    <button
+                        onClick={() => setSelectedType("Selected")}
+                        className={`btn text-sm px-3 sm:text-base sm:px-4 ${selectedType === "Selected" ? "bg-[#E7FE29]" : ""} rounded-xl sm:rounded-l-none sm:rounded-r-xl`}>
+                        Selected ({selectedPlayers.length})
+                    </button>
+                </div>
             </div>
 
-            {selectedType === "Available" ? <AvailablePlayers players={players} setCoin={setCoin} coin={coin} selectedPlayers={selectedPlayers} setSelectedPlayers={setSelectedPlayers} />
+            {/* Players Section */}
+            {selectedType === "Available"
+                ? <AvailablePlayers players={players} setCoin={setCoin} coin={coin} selectedPlayers={selectedPlayers} setSelectedPlayers={setSelectedPlayers} />
                 : <SelectedPlayers selectedPlayers={selectedPlayers} setSelectedPlayers={setSelectedPlayers} setCoin={setCoin} coin={coin} />
             }
         </div>
