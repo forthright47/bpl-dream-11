c3b208704bc7590b7fbb3d3ab99246921766eb7f
forthright47 <k.tuhinjaman@gmail.com>
2026-03-28 22:37:37 +0600

    fixed inconsistencies in the no available player box for selected players

--- src/components/SelectedPlayers/SelectedPlayers.jsx
+++ src/components/SelectedPlayers/SelectedPlayers.jsx
@@ -14,9 +14,9 @@ const SelectedPlayers = ({selectedPlayers, setSelectedPlayers, coin, setCoin}) =
     return (
         <div>
             {selectedPlayers.length === 0 ?
-            <div className='h-[250px] flex items-center justify-center flex-col gap-4 border border-gray-200 rounded-xl'>
+            <div className='h-[250px] flex items-center justify-center flex-col gap-4'>
                 <h2 className='font-semibold text-xl'>No players selected yet</h2>
-                <p>Go to Available tab to select players</p>
+                <p className='text-sm text-black/50'>Go to Available tab to select players</p>
             </div>
             : selectedPlayers.map((player, i)=>{
                 return <div key={i} className='flex items-center gap-6 justify-between p-6 rounded-2xl border my-3'>
