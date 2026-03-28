bfd171c7f27736a49fe73535e84d693ab948fa66
forthright47 <k.tuhinjaman@gmail.com>
2026-03-28 22:35:25 +0600

    added the text box of not available players if there is no selected player in the selected tab

--- src/components/SelectedPlayers/SelectedPlayers.jsx
+++ src/components/SelectedPlayers/SelectedPlayers.jsx
@@ -13,7 +13,12 @@ const SelectedPlayers = ({selectedPlayers, setSelectedPlayers, coin, setCoin}) =
 
     return (
         <div>
-            {selectedPlayers.map((player, i)=>{
+            {selectedPlayers.length === 0 ?
+            <div className='h-[250px] flex items-center justify-center flex-col gap-4 border border-gray-200 rounded-xl'>
+                <h2 className='font-semibold text-xl'>No players selected yet</h2>
+                <p>Go to Available tab to select players</p>
+            </div>
+            : selectedPlayers.map((player, i)=>{
                 return <div key={i} className='flex items-center gap-6 justify-between p-6 rounded-2xl border my-3'>
                     <div className='flex items-center gap-6'>
                         <img src={player.playerImg} alt={player.playerName} className='h-[60px] w-auto rounded-md' />
